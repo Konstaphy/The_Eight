ce22e71e889cbfb68ab2bf34d46038e833a04b18
konstaphy <konstaphy@gmail.com>
2022-02-28 02:35:54 +0500

    drags first init

--- src/app/content/content.tsx
+++ src/app/content/content.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./content.scss";
 import { config, useSpring, animated } from "react-spring";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 export const Content = () => {
   const st = useSpring({
@@ -9,9 +10,44 @@ export const Content = () => {
     config: config.molasses,
     delay: 200,
   });
+
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
   return (
     <animated.main style={st} className="content">
-      Content
+      <DragDropContext onDragEnd={() => {}}>
+        <Droppable droppableId={"1"}>
+          {(provided) => (
+            <ul
+              className="content__squares"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {numbers.map((num) => {
+                return (
+                  <Draggable
+                    draggableId={num.toString()}
+                    index={num}
+                    key={num.toString()}
+                  >
+                    {(provided) => (
+                      <li
+                        className="content__item"
+                        ref={provided.innerRef}
+                        {...provided.dragHandleProps}
+                        {...provided.draggableProps}
+                      >
+                        {num}
+                      </li>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
     </animated.main>
   );
 };
