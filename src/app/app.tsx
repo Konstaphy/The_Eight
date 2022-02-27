bc21236bb66f2eff321648a2971bb620c2bf375b
konstaphy <konstaphy@gmail.com>
2022-02-28 00:10:05 +0500

    enhancement: header disabling

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "app/app.scss";
 import { Header } from "./header/header";
 
 interface GlobalProps {}
 
 export const App: React.FC<GlobalProps> = () => {
+  const [isHeaderShown, setHeaderShown] = useState(true);
   return (
     <div className="app-container">
-      <Header />
+      <Header setHeaderShown={setHeaderShown} isHeaderShown={isHeaderShown} />
     </div>
   );
 };
--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./header.scss";
 
-interface HeaderProps {}
+interface HeaderProps {
+  setHeaderShown: (isHeaderShown: boolean) => void;
+  isHeaderShown: boolean;
+}
 
-export const Header: React.FC<HeaderProps> = () => {
-  return (
-    <div className="header">
-      <div className="header__logo">лена</div>
-    </div>
-  );
+export const Header: React.FC<HeaderProps> = (props) => {
+  useEffect(() => {
+    setTimeout(() => {
+      props.setHeaderShown(false);
+    }, 5000);
+  }, []);
+  if (props.isHeaderShown)
+    return (
+      <div className="header">
+        <div className="header__logo">С 8 марта!</div>
+      </div>
+    );
+  return <></>;
 };
