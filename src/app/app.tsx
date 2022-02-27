73db342e66a6aeef926438fe353bc09c7b16ee20
konstaphy <konstaphy@gmail.com>
2022-02-27 23:55:57 +0500

    hotfix: omg linter is stupid

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "app/app.scss";
+import { Header } from "./header/header";
 
 interface GlobalProps {}
 
 export const App: React.FC<GlobalProps> = () => {
-  return <div className="app-container">Hello world</div>;
+  return (
+    <div className="app-container">
+      <Header />
+    </div>
+  );
 };
--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -1,13 +1,12 @@
 import React from "react";
+import "./header.scss";
 
 interface HeaderProps {}
 
-const Header: React.FC<HeaderProps> = () => {
+export const Header: React.FC<HeaderProps> = () => {
   return (
     <div className="header">
-      <div className="header__logo">ЛОГО</div>
+      <div className="header__logo">лена</div>
     </div>
   );
 };
-
-export default Header;
