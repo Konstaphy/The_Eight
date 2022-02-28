3c69ed2ef13020c8df27117de0000740df0d68ae
konstaphy <konstaphy@gmail.com>
2022-03-01 02:00:50 +0500

    picture animation

--- src/app/picture/picture.tsx
+++ src/app/picture/picture.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./picture.scss";
 import test from "assets/test.jpg";
+import { animated, config, useSpring } from "react-spring";
 
 interface PictureProps {
   setPictureShown: (isPictureShown: boolean) => void;
@@ -12,10 +13,17 @@ export const Picture: React.FC<PictureProps> = (props) => {
     event.preventDefault();
   };
 
+  const st = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    config: config.molasses,
+    delay: 1000,
+  });
+
   return (
     <div className="picture-background" onClick={() => props.setPictureShown(false)}>
       <div className="picture" onClick={stopProp}>
-        <img src={test} alt="test" />
+        <animated.img style={st} src={test} alt="test" />
       </div>
     </div>
   );
